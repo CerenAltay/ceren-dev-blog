cfcdf1599a0507955047c02eec4b2f1ae5840361
Ceren <cerenaltay.lsb@gmail.com>
2022-07-14 14:50:41 +0300

    config.js edited

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  author: 'Ceren Altay', // Required
+  description: 'Full stack C# .Net Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://cerenaltay.com',
+    github: 'https://github.com/cerenaltay',
+    twitter: 'https://twitter.com/cerenmelisa_al',
+    linkedin: 'https://linkedin.com/in/ceren-altay',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+    siteUrl: 'https://github.com/CerenAltay/cerendevblog/',
 };
